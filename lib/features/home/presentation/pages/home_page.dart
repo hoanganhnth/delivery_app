23e198144e8d27a0a2cbe10bb686feed11cf050b
hoanganh <hoanganhnth2k3@gmail.com>
2026-03-25 14:38:33 +0700

    fix(home): update provider references for @riverpod migration
    - Fix provider imports and references in home page
    - Ensure compatibility with new @riverpod generated providers

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../generated/l10n.dart';
-import '../../../restaurants/presentation/providers/restaurant_providers.dart';
+import '../../../restaurants/presentation/providers/providers.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -19,7 +19,7 @@ class _HomePageState extends ConsumerState<HomePage> {
     super.initState();
     // Load featured restaurants when page initializes
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      ref.read(restaurantsNotifierProvider.notifier).loadFeaturedRestaurants();
+      ref.read(restaurantsProvider.notifier).loadFeaturedRestaurants();
     });
   }
   @override
